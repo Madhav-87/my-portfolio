3066b53ae0c78f8a6da270cc893f62ca1d652101
Madhav Bondhare <bondharemadhav8@gmail.com>
2026-04-22 10:16:20 +0530

    Add Project Folders

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header>
+        <div className='header-div'></div>
+      </header>
+    </>
+  )
+}
+
+export default App
