4b4658cadc529e6fad22a93fcef187c9330c05d6
Madhav Bondhare <bondharemadhav8@gmail.com>
2026-04-22 11:01:49 +0530

    Component Add: Header Component Created
    The Initial design of the Header Component created.
    This is just the prototype of the Header logic will be added later.

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,30 @@ function App() {
   return (
     <>
       <header>
-        <div className='header-div'></div>
+        <div className='header-div'>
+          <div className='header-box-20'>
+            <div className='profile-icon'>
+              M
+            </div>
+            <div><h3>Madhav</h3></div>
+          </div>
+          <div className='header-box-50'>
+            <div className='nav-items active-item'>Home</div>
+            <div className='nav-items'>Projects</div>
+            <div className='nav-items'>Experience</div>
+            <div className='nav-items'>Contact</div>
+          </div>
+          <div className='header-box-20'>
+            <div className='download-resume-btn'>
+              <span className="material-symbols-outlined">
+              download
+            </span>
+            <span className=''>
+              Resume
+            </span>
+            </div>
+          </div>
+        </div>
       </header>
     </>
   )
