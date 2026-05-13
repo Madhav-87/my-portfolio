a7b353d1a1805705e6b543a0501f5e83145c7ced
Madhav Bondhare <bondharemadhav8@gmail.com>
2026-05-13 19:35:31 +0530

    slider added for mobile - header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,24 +1,34 @@
 import React, { useEffect, useState } from 'react'
-import {Link }from 'react-router-dom';
-export default function Header({activeEle}) {
-    let [active,setActive]=useState("");
-    useEffect(()=>{
+import { Link } from 'react-router-dom';
+import isMobile from 'is-mobile';
+import './header.css';
+
+export default function Header({ activeEle }) {
+    let [active, setActive] = useState("");
+    useEffect(() => {
         setActive(activeEle);
-    },[]);
+    }, []);
+    let [sliderOpen,setSliderOpen]=useState(false);
+    let [mobile,setMobile]=useState(isMobile())
     return (
         <header>
             <div className='header-div'>
-                <div  className='header-box-20'>
+                <div className='header-box-20'>
                     <div className='profile-icon magic-btn'>
                         M
                     </div>
                     <div><h3>Madhav Bondhare</h3></div>
+                    <div onClick={()=>{setSliderOpen(prev=>!prev)}} className={`${mobile ? 'flex flex-col gap-10 three-dots-icon' : ''}`}>
+                        <span className={`${sliderOpen? 'lines cross-1': 'lines'}`}></span>
+                        <span className={`${sliderOpen? 'lines cross-2': 'lines'}`}></span>
+                    </div>
                 </div>
-                <div className='header-box-50'>
-                    <Link  tabindex="1" className={`nav-items color-blue-hover ${active==="Home" ? 'active-item' : ""}`} to="/">Home</Link>
-                    <Link  tabindex="2" className={`nav-items color-blue-hover ${active==="Projects" ? 'active-item' : ""}`} to="/projects">Projects</Link>
-                    <Link  tabindex="3" className={`nav-items color-blue-hover ${active==="Experience" ? 'active-item' : ""}`} to="/experience">Experience</Link>
-                    <Link  tabindex="4" className={`nav-items color-blue-hover ${active==="Contact" ? 'active-item' : ""}`} to="/contact">Contact</Link>
+
+                <div className={`${mobile? `mobile-slider ${sliderOpen? 'slide-open': 'slide-close'}`: '' } header-box-50`}>
+                    <Link tabindex="1" className={`nav-items color-blue-hover ${active === "Home" ? 'active-item' : ""}`} to="/">Home</Link>
+                    <Link tabindex="2" className={`nav-items color-blue-hover ${active === "Projects" ? 'active-item' : ""}`} to="/projects">Projects</Link>
+                    <Link tabindex="3" className={`nav-items color-blue-hover ${active === "Experience" ? 'active-item' : ""}`} to="/experience">Experience</Link>
+                    <Link tabindex="4" className={`nav-items color-blue-hover ${active === "Contact" ? 'active-item' : ""}`} to="/contact">Contact</Link>
                 </div>
             </div>
         </header>
